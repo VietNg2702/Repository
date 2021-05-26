f10c3959314c547f812a9563ca9a91902a22ca2e
VietNg2702 <49213361+VietNg2702@users.noreply.github.com>
2021-05-26 10:12:37 +0700

    update

--- 1.0.0/cores/wiring.h
+++ 1.0.0/cores/wiring.h
@@ -95,28 +95,7 @@ extern void delay( uint32_t dwMs ) ;
 
 static inline void delayMicroseconds(uint32_t) __attribute__((always_inline, unused));
 static inline void delayMicroseconds(uint32_t usec) {
-  if (usec == 0) {
-    return;
-  }
-  // TODO: Short delays at low frequencies.
-  uint64_t current, later;
-  rdmcycle(&current);
-  later = current + usec * (F_CPU/1000000);
-  if (later > current) // usual case
-    {
-      while (later > current) {
-	rdmcycle(&current);
-      }
-    }
-  else // wrap. Though this is unlikely to be hit w/ 64-bit mcycle
-    {
-      while (later < current) {
-	rdmcycle(&current);
-      }
-      while (current < later) {
-	rdmcycle(&current);
-      }
-    }
+	//TODO:
 }
 
   
