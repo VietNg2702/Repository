4e5f82d4dbc9bbfa88b237eaad433d8d6bc128e0
VietNg2702 <ndviet.bkhcm@gmail.com>
2021-06-22 10:41:28 +0700

    update

---
+++ 1.0.0/libraries/lr1110/examples/application/gui/guiAbout.cpp
@@ -0,0 +1,110 @@
+/**
+ * @file      guiAbout.cpp
+ *
+ * @brief     Implementation of the gui about page.
+ *
+ * Revised BSD License
+ * Copyright Semtech Corporation 2020. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Semtech corporation nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL SEMTECH CORPORATION BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "guiAbout.h"
+#include "../version.h"
+#include "semtech_logo.h"
+
+#define GUI_ABOUT_BUFFER_LENGTH ( 100 )
+
+GuiAbout::GuiAbout( version_handler_t* version_handler )
+    : GuiCommon( GUI_PAGE_ABOUT ), version_handler( version_handler )
+{
+    char buffer[GUI_ABOUT_BUFFER_LENGTH];
+
+    this->createHeader( "ABOUT" );
+
+    snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "EVK demo app -  %s", DEMO_VERSION );
+    this->createSection( buffer, -100 );
+
+    switch( this->version_handler->device_type )
+    {
+    case VERSION_DEVICE_TRANSCEIVER:
+    {
+        snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "LR1110 type - 0x%02x",
+                  this->version_handler->transceiver.version_chip_type );
+        this->createSection( buffer, -80 );
+
+        snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "LR1110 hardware - 0x%02x",
+                  this->version_handler->transceiver.version_chip_hw );
+        this->createSection( buffer, -60 );
+
+        snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "LR1110 firmware - 0x%04x",
+                  this->version_handler->transceiver.version_chip_fw );
+        this->createSection( buffer, -40 );
+        break;
+    }
+
+    case VERSION_DEVICE_MODEM:
+    {
+        snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "Modem type - 0x%02x",
+                  this->version_handler->modem.version_chip_type );
+        this->createSection( buffer, -80 );
+
+        snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "Modem bootloader - 0x%08x",
+                  this->version_handler->modem.version_chip_bootloader );
+        this->createSection( buffer, -60 );
+
+        snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "Modem firmware - 0x%06x",
+                  this->version_handler->modem.version_chip_fw );
+        this->createSection( buffer, -40 );
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "LVGL\nCopyright (c) 2016\nGabor Kiss-Vamosi" );
+    this->createSection( buffer, 10 );
+
+    snprintf( buffer, GUI_ABOUT_BUFFER_LENGTH, "Data (c) OpenStreetMap\ncontributors, ODbL 1.0." );
+    this->createSection( buffer, 80 );
+
+    this->createActionButton( &( this->btn_back ), "BACK", GuiAbout::callback, GUI_BUTTON_POS_CENTER, -5, true );
+
+    lv_scr_load( this->screen );
+}
+
+GuiAbout::~GuiAbout( ) {}
+
+void GuiAbout::callback( lv_obj_t* obj, lv_event_t event )
+{
+    GuiAbout* self = ( GuiAbout* ) lv_obj_get_user_data( obj );
+
+    if( event == LV_EVENT_RELEASED )
+    {
+        if( obj == self->btn_back )
+        {
+            GuiCommon::_event = GUI_EVENT_BACK;
+        }
+    }
+}
---
+++ 1.0.0/libraries/lr1110/examples/application/hci/Command/command_start_demo.h
@@ -0,0 +1,65 @@
+/**
+ * @file      command_start_demo.h
+ *
+ * @brief     Definitions of the HCI command to start demo class.
+ *
+ * Revised BSD License
+ * Copyright Semtech Corporation 2020. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Semtech corporation nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL SEMTECH CORPORATION BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __COMMAND_START_DEMO_H__
+#define __COMMAND_START_DEMO_H__
+
+#include "command_base.h"
+#include "../hci.h"
+#include "../../demo/demo_manager_interface.h"
+
+class CommandStartDemo : public CommandBase
+{
+   public:
+    explicit CommandStartDemo( DeviceInterface* device, Hci& hci, DemoManagerInterface& demo_holder );
+    virtual ~CommandStartDemo( );
+
+    virtual uint16_t GetComCode( );
+    virtual bool     ConfigureFromPayload( const uint8_t* buffer, const uint16_t buffer_size );
+    virtual bool     Job( );
+
+   protected:
+    bool ConfigureWifiScan( const uint8_t* buffer, const uint16_t buffer_size );
+    bool ConfigureWifiCountryCode( const uint8_t* buffer, const uint16_t buffer_size );
+    bool ConfigureGnssAutonomous( const uint8_t* buffer, const uint16_t buffer_size );
+    bool ConfigureGnssAssisted( const uint8_t* buffer, const uint16_t buffer_size );
+    bool ConfigureGnss( demo_gnss_settings_t* gnss_setting, const uint8_t* buffer, const uint16_t buffer_size );
+
+    static demo_wifi_mode_t             wifi_mode_from_value( const uint8_t& value );
+    static demo_wifi_signal_type_scan_t wifi_signal_type_scan_from_val( const uint8_t& val );
+
+   private:
+    CommandBaseDemoId_t   demo_id_to_start;
+    demo_all_settings_t   demo_settings;
+    DemoManagerInterface& demo_holder;
+};
+
+#endif  // __COMMAND_START_DEMO_H__
